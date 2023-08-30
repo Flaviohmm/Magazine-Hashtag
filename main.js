86b10ddc0ab1b8dc9751a8da90fdb15e70dca847
Flavio Macedo <fhenrique609@gmail.com>
2023-08-30 13:56:55 -0300

    Listagem de Produtos usando o for

--- main.js
+++ main.js
@@ -1,18 +1,81 @@
-const produto1 = {
-    nome: "Casaco Branco",
+const catalogo = [
+  {
+    id: 1,
     marca: "Zara",
+    nome: "Camisa Larga com Bolsos",
     preco: 70,
-    nomeArquivoImagem: "product-1.jpg",
-};
+    imagem: "product-1.jpg",
+    feminino: false,
+  },
+  {
+    id: 2,
+    marca: "Zara",
+    nome: "Casaco Reto com Lã",
+    preco: 85,
+    imagem: "product-2.jpg",
+    feminino: true,
+  },
+  {
+    id: 3,
+    marca: "Zara",
+    nome: "Jaqueta com Efeito Camurça",
+    preco: 60,
+    imagem: "product-3.jpg",
+    feminino: false,
+  },
+  {
+    id: 4,
+    marca: "Zara",
+    nome: "Sobretudo em Mescla de Lã",
+    preco: 160,
+    imagem: "product-4.jpg",
+    feminino: false,
+  },
+  {
+    id: 5,
+    marca: "Zara",
+    nome: "Camisa Larga Acolchoada de Veludo Cotelê",
+    preco: 110,
+    imagem: "product-5.jpg",
+    feminino: false,
+  },
+  {
+    id: 6,
+    marca: "Zara",
+    nome: "Casaco de Lã com Botões",
+    preco: 170,
+    imagem: "product-6.jpg",
+    feminino: true,
+  },
+  {
+    id: 7,
+    marca: "Zara",
+    nome: "Casaco com Botões",
+    preco: 75,
+    imagem: "product-7.jpg",
+    feminino: true,
+  },
+  {
+    id: 8,
+    marca: "Zara",
+    nome: "Colete Comprido com Cinto",
+    preco: 88,
+    imagem: "product-8.jpg",
+    feminino: true,
+  },
+];
 
-const cartaoProduto = /* html */
-`<div id="card-produto-1">
-    <img src="./assets/img/${produto1.nomeArquivoImagem}" alt="Produto 1 do Magazine Hashtag." style="height: 300px;">
-    <p>${produto1.marca}</p>
-    <p>${produto1.nome}</p>
-    <p>${produto1.preco}</p>
-    <button>Adicionar</button>
-</div>
-`;
+for(const produtoCatalogo of catalogo) {
+    const cartaoProduto =
+      /* html */
+      `<div id="card-produto-1">
+        <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto 1 do Magazine Hashtag." style="height: 300px;">
+        <p>${produtoCatalogo.marca}</p>
+        <p>${produtoCatalogo.nome}</p>
+        <p>$${produtoCatalogo.preco}</p>
+        <button>Adicionar</button>
+    </div>
+    `;
 
-document.getElementById("container-produto").innerHTML += cartaoProduto;
+    document.getElementById("container-produto").innerHTML += cartaoProduto;
+}
