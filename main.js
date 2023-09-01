0452fd52fc2ace772280af1462a8620c296764c2
Flavio Macedo <fhenrique609@gmail.com>
2023-08-31 23:29:37 -0300

    Criando header e add icons

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ for(const produtoCatalogo of catalogo) {
     const cartaoProduto =
       /* html */
     `
-        <div id="card-produto-1">
+        <div class="border-solid border-2 border-sky-500 w-48 m-2" id="card-produto-${produtoCatalogo.id}">
             <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto 1 do Magazine Hashtag." style="height: 300px;">
             <p class="marca">${produtoCatalogo.marca}</p>
             <p>${produtoCatalogo.nome}</p>
